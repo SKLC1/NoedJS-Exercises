b99e03762dd901519be3dc8d2f716852839d928b
SKLC1 <segevklein2000@gmail.com>
2022-06-15 17:21:23 +0300

    more exrecises

--- Exercise11.2/myBackend/app.js
+++ Exercise11.2/myBackend/app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import axios from 'axios'
-
+import cors from 'cors'
 const app = express()
 app.use(express.json())
+app.use(cors())
 const PORT = 5000;
 
 app.get('/getWeatherIsrael', async(req,res) => {
